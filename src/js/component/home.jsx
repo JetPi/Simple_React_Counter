43b11f867fdd46a0fafc7cef6a0b633903fea261
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-07-19 18:28:25 -0400

    Done, may consider looking into that gray shadow in the back of numbers

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
 import React from "react";
+import PropType from "prop-types"
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="container-fluid">
+			<div className="row bg-dark">
+				<div className="col-2 bg-dark text-light fs-1">
+				<i className="fa-regular fa-clock" style={{color: "solid white"}}></i>
+				</div>
+				<div className="col-2 bg-dark text-light fs-1">
+				{props.num5 % 10}
+				</div>
+				<div className="col-2 bg-dark text-light fs-1">
+				{props.num4 % 10}
+				</div>
+				<div className="col-2 bg-dark text-light fs-1">
+				{props.num3 % 10}
+				</div>
+				<div className="col-2 bg-dark text-light fs-1">
+				{props.num2 % 10}
+				</div>
+				<div className="col-2 bg-dark text-light fs-1">
+				{props.num1 % 10}
+				</div>
+			</div>
 		</div>
+		</>
 	);
 };
+
+Home.propTypes ={
+	num1: PropType.number,
+	num2: PropType.number,
+	num3: PropType.number,
+	num4: PropType.number,
+	num5: PropType.number,
+}
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,27 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+// let [seconds, setSeconds] = useState(0);
+// useEffect(() => {
+//     let cancel = setInterval(setSeconds(seconds => seconds + 1), 1000)
+//     return () => clearInterval(cancel);
+// }, []);
+
+
+let counter = 0;
+
+
+setInterval( ()=> {
+    let num1 = Math.floor(counter/1)
+    let num2 = Math.floor(counter/10)
+    let num3 = Math.floor(counter/100)
+    let num4 = Math.floor(counter/1000)
+    let num5 = Math.floor(counter/10000)
+
+    counter++
+    ReactDOM.render(<Home num1={num1} num2={num2} num3={num3} num4={num4} num5={num5} />, document.querySelector("#app"));
+}, 1000)
+
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
